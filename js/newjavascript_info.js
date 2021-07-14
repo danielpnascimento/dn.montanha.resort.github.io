900e5cbc72308ded9f72670877a2cdebed14d1c0
Daniel Nascimento <daniel.p.nasciment@gmail.com>
2021-07-14 11:50:22 -0300

    Up

---
+++ js/newjavascript_info.js
@@ -0,0 +1,14 @@
+
+
+var tab = new Swiper('.tab-content-valores', {
+    speed: 1200,
+    slidesPerView: 1,
+    grabCursor: true,
+
+    thumbs: {
+        swiper: {
+            el: '.tab-menu-valores',
+            slidesPerView: 4
+        }
+    }
+});
